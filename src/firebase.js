2122914136cb4d56e7aeb82bdd537d62189c8c0f
supuna98 <supunawa@gmail.com>
2020-10-19 18:45:18 +0530

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDdDWyA2YguAt5PX9d8jOKTm1NXuZivCQU",
-    authDomain: "whatsapp-clone-d1a5d.firebaseapp.com",
-    databaseURL: "https://whatsapp-clone-d1a5d.firebaseio.com",
-    projectId: "whatsapp-clone-d1a5d",
-    storageBucket: "whatsapp-clone-d1a5d.appspot.com",
-    messagingSenderId: "207871271553",
-    appId: "1:207871271553:web:78820589f5114b00466380",
-    measurementId: "G-63ET0XDHM2"
+    // in here add your firebase config details
   };
 
 
